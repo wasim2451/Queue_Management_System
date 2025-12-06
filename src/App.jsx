ed4324dedcf6a787dbaf1d64587e539877e1586a
Wasim Akhtar <wasimakhtarkhan2003@gmail.com>
2025-12-06 11:36:09 +0530

    Added Buttons for Update Status and Delete Queue items . Working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import QueueForm from './components/QueueForm';
-import DisplayFunc  from './components/DisplayFunc';
-export default function App(){
-  const [queue,setQueue]=useState([]);
+import { useState } from "react";
+import "./App.css";
+import QueueForm from "./components/QueueForm";
+import DisplayFunc from "./components/DisplayFunc";
+export default function App() {
+  const [queue, setQueue] = useState([]);
 
-  const addToQueue =(data)=>{
+  const addToQueue = (data) => {
     // Queue will be ADDED
-    setQueue([...queue,{...data,
-      id:Date.now(),
-      status:"Waiting"
-    }]);
+    setQueue([...queue, { ...data, id: Date.now(), status: "Waiting" }]);
     // console.log(queue);
-    // It is async so states does not get immediatley updated 
-  }
-  const removeFromQueue =(id)=>{
+    // It is async so states does not get immediatley updated
+  };
+  const removeFromQueue = (id) => {
     // Queue will be REMOVED
-    setQueue(queue.filter(item=>item.id!==id));
-  }
-  const updateQueueStatus =(id,newStatus)=>{
+    setQueue(queue.filter((item) => item.id !== id));
+  };
+  const updateQueueStatus = (id, newStatus) => {
     // Queue will be UPDATED
-    setQueue(queue.map((item)=>{
-      if(item.id==id){
-        return{...item,status:newStatus}
-      }
-      return item;
-    }));
-  }
+    setQueue(
+      queue.map((item) => {
+        if (item.id == id) {
+          return { ...item, status: newStatus };
+        }
+        return item;
+      })
+    );
+  };
 
-  
-  return <div>
-    <header>
-      <h1 className="header">Queue Management System</h1>
-      <p className="subheader">Manage your customer efficiently</p>
-    </header>
-    <main>
-      {/* Two Components will be there  */}
-      {/* Queue Form  + Display Component*/}
-      <QueueForm onAdd={addToQueue}/>
-      <DisplayFunc queue={queue} onUpdate={updateQueueStatus} onRemove={removeFromQueue}/>
-    </main>
-
-  </div>
+  return (
+    <div style={{ marginTop: "60px" }}>
+      <header style={{ lineHeight: "20px" }}>
+        <h1 className="header">Queue Management System</h1>
+        <p className="subheader">Manage your customer efficiently</p>
+      </header>
+      <main>
+        {/* Two Components will be there  */}
+        {/* Queue Form  + Display Component*/}
+        <QueueForm onAdd={addToQueue} />
+        <DisplayFunc
+          queue={queue}
+          onUpdate={updateQueueStatus}
+          onRemove={removeFromQueue}
+        />
+      </main>
+    </div>
+  );
 }
--- src/components/DisplayFunc.jsx
+++ src/components/DisplayFunc.jsx
@@ -1,15 +1,67 @@
-export default function DisplayFunc({queue,onUpdate,onRemove}){
-    
-    
-    return (
-        <div className="display-container">
-            <h2 className="header" style={{"color":"#ffff"}}>Current Queue</h2>
-            <div>
-            {
-               queue.length==0 ? (<p className="text-normal">No Queue Present</p>) : (<div className="text-normal">Yes Queue Present</div>) 
-            }
-            </div>
-            
-        </div>
-    )
+import { FaUserTimes } from "react-icons/fa";
+export default function DisplayFunc({ queue, onUpdate, onRemove }) {
+  function getColor(status) {
+    switch (status) {
+      case "Waiting":
+        return "#ff0000";
+      case "Serving":
+        return "#00ff00";
+      case "Complete":
+        return "#646cff";
+      default:
+        return "#ffff";
+    }
+  }
+
+  return (
+    <div className="display-container">
+      <h2 className="header" style={{ color: "#ffff" }}>
+        Current Queue
+      </h2>
+      <div className="display-div">
+        {queue.length == 0 ? (
+          <p className="serviceText">No Queue Present</p>
+        ) : (
+          <div className="display-div">
+            {queue.map((customer) => (
+              <div key={customer.id} className="div-card">
+                <div>
+                  <p className="textList">{customer.name}</p>
+                  <p className="serviceText"> Service : {customer.services}</p>
+                  <p style={{ color: getColor(customer.status) }}>
+                    {customer.status}
+                  </p>
+                </div>
+                <div className="card-btn-container">
+                  {/* Button to remove user */}
+                  {customer.status == "Waiting" && (
+                    <button
+                      className="serveBtn"
+                      onClick={() => onUpdate(customer.id, "Serving")}
+                    >
+                      Serve
+                    </button>
+                  )}
+                  {customer.status == "Serving" && (
+                    <button
+                      className="completeBtn"
+                      onClick={() => onUpdate(customer.id, "Complete")}
+                    >
+                      Complete
+                    </button>
+                  )}
+                  <button
+                    className="delete-btn"
+                    onClick={() => onRemove(customer.id)}
+                  >
+                    <FaUserTimes />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
